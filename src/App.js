e15f4d515428f86fec9f29a9ee0f7768ba031089
Gauravkhatiwada <132439835+Gauravkhatiwada@users.noreply.github.com>
2025-01-13 18:40:33 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>GAURAVKHATIWDAISMYNAME</code> and save to reload.
+          <code>GAURAV KHATIWDA IS MY NAME</code> 
         </p>
         <a
           className="App-link"
