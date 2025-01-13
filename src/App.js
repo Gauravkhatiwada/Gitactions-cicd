2cb17453dfb93909bd4a85eba0509eb8a62a7b43
Gauravkhatiwada <132439835+Gauravkhatiwada@users.noreply.github.com>
2025-01-13 23:38:11 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>GAURAV KHATIWDA IS sexy</code> 
+          <code>(GAURAV-KHATIWDA)</code> 
         </p>
         <a
           className="App-link"
